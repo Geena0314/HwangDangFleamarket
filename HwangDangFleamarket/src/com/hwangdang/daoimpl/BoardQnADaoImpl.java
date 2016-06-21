86f4348779c5056c48bdf9542d41a94441b00496
jihoonpark <jjjhhhvvv@gmail.com>
2016-06-21 11:11:36 +0900

    메인페이지변경  -A태그 txmanager 추가 @Transactional 어노테이션 추가 QnABoardController 오류 수정

--- HwangDangFleamarket/src/com/hwangdang/daoimpl/BoardQnADaoImpl.java
+++ HwangDangFleamarket/src/com/hwangdang/daoimpl/BoardQnADaoImpl.java
@@ -3,16 +3,20 @@ package com.hwangdang.daoimpl;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.hwangdang.dao.Dao;
 
+@Repository
 public class BoardQnADaoImpl implements Dao {
 
+	@Autowired
 	private SqlSessionTemplate session;
 	
-	public BoardQnADaoImpl(SqlSessionTemplate session){
+/*	public BoardQnADaoImpl(SqlSessionTemplate session){
 		this.session = session;
-	}
+	}*/
 	
 	//QnA게시판 전체 조회
 	public List selectAllQnABoard(){
